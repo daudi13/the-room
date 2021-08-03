5442efa82f85c6a435954f6c9c2784a1ff19ce15
daudi13 <davouma13@gmail.com>
2021-08-03 05:54:20 +0300

    commit update

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const hamberger = document.querySelector(".humberger");
+const hambuger = document.querySelector(".hamberger");
 const close = document.querySelector(".close");
-const navMobile = document.querySelector(".mobile-nav");
-const narration = document.querySelector(".narration");
+const mobileNav = document.querySelector(".mobile-nav");
+const narration = document.querySelectorAll(".narration");
 const hero = document.querySelector(".hero");
 
 let currentNarration = 0;
 
-hamberger.addEventListener("click", function () {
-    navMobile.classList.add("drop-nav");
-    navMobile.classList.remove("drop-nav");
+hambuger.addEventListener("click", function () {
+    mobileNav.classList.add("drop-nav");
+    mobileNav.classList.remove("close-nav");
 });
 
 close.addEventListener("click", function () {
-    navMobile.classList.remove("drop-nav");
-    navMobile.classList.add("drop-nav");
+    mobileNav.classList.remove("drop-nav");
+    mobileNav.classList.add("close-nav");
 });
+
 
 showNarration(currentNarration);
 
 function showNarration(x) {
-    narration[x].style.display = "block"
-};
+    narration[x].style.display = "block";
+
+}
 
 function prevNext(x) {
+
     narration[currentNarration].style.display = "none";
 
     currentNarration = currentNarration + x;
@@ -35,16 +38,13 @@ function prevNext(x) {
     }
 
     if (window.screen.availWidth <= 540) {
-        hero.style.backgroundImage = `url("/images/moblie-image-hero-${currentNarration + 1}.jpg")`;
+        hero.style.backgroundImage = `url("images/mobile-image-hero-${currentNarration + 1}.jpg")`;
     }
-
-    else if (window.screen.availWidth > 540) {
-        hero.style.backgroundImage = `url("/images/desktop-image-hero-${currentNarration + 1}.jpg")`;
+    else if(window.screen.availWidth > 540){
+        hero.style.backgroundImage = `url("images/desktop-image-hero-${currentNarration + 1}.jpg")`;
     }
 
     narration[currentNarration].style.display = "block";
 
     showNarration(currentNarration);
-
-
 }
